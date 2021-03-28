c975193b08027fb2a045de08ea9a27e3e8684016
cfconor <cfconor@gmail.com>
2021-03-28 21:48:02 +0000

    updated connection script

--- mySql.py
+++ mySql.py
@@ -11,10 +11,12 @@ connection = pymysql.connect(host='localhost', user=username, db='Chinook')
 try:
     # Run a query
     with connection.cursor() as cursor:
-        sql = "SELECT * FROM Artist;"
-        cursor.execute(sql)
-        result = cursor.fetchall()
-        print(result)
+        rows = [("Bob", 21, "1990-03-25 13:23:54"),
+                ("Jim", 331, "1990-03-22 13:23:54"),
+                ("Fred", 31, "1990-03-21 13:23:54")]
+        cursor.executemany("""INSERT INTO Friends VALUES (%s, %s, %s);""",
+                           rows)
+        connection.commit()
 finally:
     # close the connection
     connection.close()
