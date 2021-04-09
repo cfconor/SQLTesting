e9b33fe1c856935b3953240a709ee44807b52ff0
cfconor <cfconor@gmail.com>
2021-04-09 10:13:58 +0000

    partially completed profile page functionality

--- app.py
+++ app.py
@@ -75,6 +75,13 @@ def login():
     return render_template("login.html")
 
 
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
+
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
